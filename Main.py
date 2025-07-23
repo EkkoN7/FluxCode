807f11d86fa5d81074d96d88555053feac3ec8a4
Denis Durban <CyberSpear@outlook.de>
2025-07-23 21:41:33 +0200

    update

--- Main.py
+++ Main.py
@@ -1,8 +1,6 @@
-from functions import get_random_number, get_random_list, encrypt_msg, decrypt_msg
-from shift_list import letters, numbers, symbols, allchars, acii_art
+from functions import password_seed, encrypt_msg, decrypt_msg
+from shift_list import allchars, acii_art
 
-enum = get_random_number()
-encrypt_list = get_random_list(enum = enum, letters = letters, sym = symbols, num=numbers)
 print (acii_art)
 is_unsecure = True
 
@@ -10,29 +8,25 @@ while is_unsecure:
     user_input = input("Welcome. Do you want to (d)ecrypt or (e)encrypt your message: ").lower().strip()
     user_input_isalpha = user_input.isalpha()
     if user_input_isalpha == True and user_input == "e":
-        user_msg = input("Enter your Message: ")
-        shift_number =  input("Enter shift number from 1 to 20: ")
-        shift_number_isdigit = shift_number.isdigit()
-        if int(shift_number) <= 20 and shift_number_isdigit == True:
-            enum = get_random_number()
-            new_list = get_random_list(enum = enum , letters = letters, sym = symbols, num = numbers)
-            message = encrypt_msg(msg = user_msg , index_list = allchars, new_list = new_list, shift_num = shift_number)
-            print(f"Your encrypted message is:\n{message}\nYour secret key is: {enum}. To decrypt your message, please remember both the shift number you chose and this secret key.")
+        user_msg = input("Enter your Message: ").strip()
+        user_pwd = input("Password (12-20 characters required): ").strip()
+        if len(user_pwd) <= 20 and len(user_pwd) >= 12:
+            new_list = password_seed(allchars, user_pwd)
+            message = encrypt_msg(msg = user_msg , index_list = allchars, new_list = new_list)
+            print(f"Your encrypted message is:\n{message}\nTo decrypt your message, please remember both the password you chose and your encrypted message.")
             is_unsecure = False
         else:
-            print("Please enter a valid shift number between 1 to 20.")
+            print("Please enter a valid password.")
 
     elif user_input_isalpha == True and user_input == "d":
-        user_msg = input("Enter Message: ")
-        shift_number =  input("Enter shift number from 1 to 20: ")
-        shift_number_isdigit = shift_number.isdigit()
-        if int(shift_number) <= 20 and shift_number_isdigit == True:
-            enum = input("Enter secret number: ")
-            new_list = get_random_list(enum=enum, letters=letters, sym=symbols, num=numbers)
-            message = decrypt_msg(msg=user_msg, index_list=allchars, new_list=new_list, shift_num=shift_number)
+        user_msg = input("Enter Message: ").strip()
+        user_pwd =  input("Enter password: ").strip()
+        if len(user_pwd) <= 20 and len(user_pwd) >= 12:
+            new_list = password_seed(allchars, user_pwd)
+            message = decrypt_msg(msg=user_msg, index_list=allchars, new_list=new_list)
             print(f"Your decrypted message is:\n{message}")
             is_unsecure = False
         else:
-            print("Please enter a valid shift number between 1 to 20.")
+            print("Please enter a valid password.")
     else:
         print("Please choose either to (e)ncrypt or (d)ecrypt a message")
--- functions.py
+++ functions.py
@@ -1,45 +1,29 @@
 import random
 
-def get_random_number():
-    random_number = random.randint(1000, 9999)
-    random_division = random.randint(100, 999)
-    random_list = list(str(random_number//random_division))
-    if len(random_list) > 1:
-        encrypt_num = random_list[1]
-        return int(encrypt_num)
-    else:
-        encrypt_num = random_list[0]
-        return int(encrypt_num)
+def password_seed(allchars, user_password: str):
+    random.seed(user_password)
+    new_list = random.sample(allchars, len(allchars))
+    return new_list
 
-def get_random_list(enum, letters, sym, num):
-    if enum == 0 or enum == 3 or enum == 6:
-        return sym
-    elif enum == 1 or enum == 4 or enum == 7:
-        return num
-    elif enum == 2 or enum == 5 or enum == 8 or enum == 9:
-        return letters
-    else:
-        return letters
-
-def encrypt_msg(msg, index_list, new_list, shift_num):
+def encrypt_msg(msg, index_list, new_list):
     encrypted_msg = []
     for char in msg:
         if char == " ":
             encrypted_msg.append(" ")
         else:
             letters_index = index_list.index(char)
-            shift_list_index = new_list[int(letters_index) + int(shift_num)]
-            encrypted_msg.append(shift_list_index)
+            random_list = new_list[int(letters_index)]
+            encrypted_msg.append(random_list)
     return "".join(encrypted_msg)
 
-def decrypt_msg(msg, index_list, new_list, shift_num):
+def decrypt_msg(msg, index_list, new_list):
     decrypted_msg = []
     for char in msg:
         if char == " ":
             decrypted_msg.append(" ")
         else:
             letters_index = new_list.index(str(char))
-            shift_list_index = index_list[int(letters_index) - int(shift_num)]
-            decrypted_msg.append(shift_list_index)
+            random_list = index_list[int(letters_index)]
+            decrypted_msg.append(random_list)
     return "".join(decrypted_msg)
 
